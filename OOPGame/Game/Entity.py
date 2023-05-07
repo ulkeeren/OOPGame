eef9691ca42f1fc3ccedb9f152269632baf98aa5
ulkeeren <ulkeeren@gmail.com>
2023-05-07 12:03:43 +0300

    biddi

--- OOPGame/Game/Entity.py
+++ OOPGame/Game/Entity.py
@@ -1,4 +1,6 @@
 from asyncio.windows_events import NULL
+from math import sqrt
+from tkinter.font import NORMAL
 import pygame
 class Entity:
     screen=NULL
@@ -13,6 +15,8 @@ class Entity:
         self.screen=screen
     def printIt(self):
         self.screen.blit(self.image,(self.x_coor,self.y_coor))
+    def getRect(self):
+        return self.image.get_rect()
 class Gunslinger(Entity):
     def __init__(self,x_coor,y_coor,image_path,screen):
         super().__init__(x_coor,y_coor,image_path,screen)
@@ -39,18 +43,24 @@ class Gunslinger(Entity):
             self.screen=screen
             self.image=pygame.transform.scale(pygame.image.load(r"C:\Users\user\source\repos\OOPGame\OOPGame\other\bullet.png"),(10,10))
         def update(self,dt):
-            self.cpx+=(self.dx-self.px)*dt
-            self.cpy+=(self.dy-self.py)*dt
-        def checkCollision(self):
-            pass
+            xwide=(self.dx-self.px)
+            ywide=(self.dy-self.py)
+            normalization=sqrt((xwide**2) + (ywide**2))
+            self.cpx+=((self.dx-self.px)/normalization)*1200*dt
+            self.cpy+=((self.dy-self.py)/normalization)*1200*dt
+        def checkCollision(self,zomb):
+            if pygame.Rect((self.cpx,self.cpy),(10,10)).colliderect(pygame.Rect((zomb.x_coor,zomb.y_coor),(100,90))):
+                zomb.dead=True
         def printIt(self):
             self.screen.blit(self.image,(self.cpx,self.cpy))
      
     def shoot_bullet(self,dx,dy):
         self.bullets.append(self.Bullet(dx,dy,self.x_coor,self.y_coor,self.screen))
-    def checkBulletCollision(self):
+        if dx>self.x_coor:
+            pygame.transform.flip(self.image,False,True)
+    def checkBulletCollision(self,zomb):
         for bullet in self.bullets:
-            bullet.checkCollision()
+            bullet.checkCollision(zomb)
     def update(self,keys,dt):
         if keys[pygame.K_w]:
            self.y_coor -= 300 * dt
@@ -63,3 +73,18 @@ class Gunslinger(Entity):
         for bullet in self.bullets:
             bullet.printIt()
             bullet.update(dt)
+class Zombie(Entity):
+    def __init__(self,x_coor,y_coor,image_path,screen):
+        super().__init__(x_coor,y_coor,image_path,screen)
+        self.image=pygame.transform.scale(self.image,(100,90))
+    dead=False
+    def update(self,px,py,dt):
+        if not self.dead:
+            xwide=(self.x_coor-px)
+            ywide=(self.y_coor-py)
+            normalization=sqrt((xwide**2) + (ywide**2))
+            self.x_coor-=((self.x_coor-px)/normalization)*300*dt
+            self.y_coor-=((self.y_coor-py)/normalization)*300*dt
+        else:
+            pygame.transform.rotate(self.image,90)
+        self.printIt()
--- OOPGame/Game/OOPGame.py
+++ OOPGame/Game/OOPGame.py
@@ -1,10 +1,11 @@
 import pygame
 import random
 from Entity import *
-#initializing the arrays containing all things related to the entities and their images
-level=1
-things=[]
-
+zombieTimer=0
+xrandom=random.seed(420)
+yrandom=random.seed(69)
+zombieCounter=0
+zombies=[]
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -13,22 +14,27 @@ running = True
 dt = 0
 #initializing the player and appending it to the arrays
 player=Gunslinger(250,250,r"C:\Users\user\source\repos\OOPGame\OOPGame\other\gunslinger.png",screen)
-things.append(player)
 while running:
+    zombieTimer+=dt
+    if zombieTimer>2:
+        zombies.append(Zombie(random.randint(0,1280),random.randint(0,720),r"C:\Users\user\source\repos\OOPGame\OOPGame\other\zomb.png",screen))
+        zombieTimer=0
     time_passed+=clock.tick()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill("black")
-    for thing in things:
-        thing.printIt()
     mousePressed=pygame.mouse.get_pressed()
     mousePos=pygame.mouse.get_pos()
-    if any(mousePressed) and time_passed>0.2:
+    if any(mousePressed) and time_passed>1:
         player.shoot_bullet(mousePos[0],mousePos[1])
         time_passed=0
     keys = pygame.key.get_pressed()
     player.update(keys,dt)
+    player.printIt()
+    for zombie in zombies:
+        player.checkBulletCollision(zombie)
+        zombie.update(player.x_coor,player.y_coor,dt)
     pygame.display.flip()
     dt = clock.tick(60) / 1000
     time_passed+=dt
