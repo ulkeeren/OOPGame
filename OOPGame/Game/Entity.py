f5ad3633cb5e51d49e936f6dc1703c7c466a25ba
ulkeeren <ulkeeren@gmail.com>
2023-05-07 09:28:38 +0300

    zattirizortzort

--- OOPGame/Game/Entity.py
+++ OOPGame/Game/Entity.py
@@ -8,11 +8,10 @@ class Entity:
     def __init__(self,x_coor,y_coor,image_path,screen):
         self.x_coor=x_coor
         self.y_coor=y_coor
-        self.image=pygame.transform.scale(pygame.image.load(image_path),(160,147))
+        self.image=pygame.transform.scale(pygame.image.load(image_path),(100,90))
         self.screen=screen
-    rect=pygame.Rect(x_coor,y_coor,160,147)
     def printIt(self):
-        self.screen.blit(self.image,(self.x_coor,self.y_coor)).convert()
+        self.screen.blit(self.image,(self.x_coor,self.y_coor))
 class Gunslinger(Entity):
     def __init__(self,x_coor,y_coor):
         super().__init__(self,x_coor,y_coor)
@@ -28,7 +27,6 @@ class Gunslinger(Entity):
         px=NULL
         py=NULL
 
-        image=pygame.image.load("C:\Users\user\source\repos\OOPGame\OOPGame\other\bullet.png").convert()
         screen=NULL
         def __init__(self,dx,dy,px,py,screen):
             self.dx=dx
@@ -36,15 +34,16 @@ class Gunslinger(Entity):
             self.px=px
             self.py=py
             self.screen=screen
-        def update(self,dt):
-            self.px=(self.dx-self.px)*dt
-            self.py=(self.dy-self.py)*dt
-        def checkCollision(self):
-            pass
-        def printIt(self):
-            self.screen.blit(self.image,(self.px,self.py))
-    def shoot_bullet(self,dx,dy):
-        self.bullets.append(self.Bullet(dx,dy,self.x_coor,self.y_coor,self.screen))
-    def checkBulletCollision(self):
-        for bullet in self.bullets:
-            bullet.checkCollision()
+        #image=pygame.image.load("C:\Users\user\source\repos\OOPGame\OOPGame\other\bullet.png")
+#        def update(self,dt):
+#            self.px=(self.dx-self.px)*dt
+#            self.py=(self.dy-self.py)*dt
+#        def checkCollision(self):
+#            pass
+#        def printIt(self):
+#            self.screen.blit(self.image,(self.px,self.py))
+#    def shoot_bullet(self,dx,dy):
+#        self.bullets.append(self.Bullet(dx,dy,self.x_coor,self.y_coor,self.screen))
+#    def checkBulletCollision(self):
+#        for bullet in self.bullets:
+#            bullet.checkCollision()
